e1d94263ecb947a3be0efc9fbb1f699047cf261c
alik15 <ali.mkhalid@gmail.com>
2023-08-21 22:45:09 +0500

    day 2

---
+++ 2022/day2/lib.py
@@ -0,0 +1,34 @@
+
+
+def check(a,b):
+    pass 
+
+
+
+
+
+
+
+
+
+
+
+def points(given):
+    if (given == "Rock"):
+        return 1 
+    elif (given =="Paper"):
+        return 2
+    elif( given == "Scissors"):
+        return 3
+
+
+
+
+
+def map(choice):
+    if (choice == ("A" or "X")):
+        return "Rock"
+    elif(choice ==("B" or "Y"))
+        return "Paper"
+    elif(choice == ("C" or "Z")):
+        return "Scissors"
