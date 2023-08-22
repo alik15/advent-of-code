02d2b382539e8918ccf62db186239fba8757b80e
alik15 <ali.mkhalid@gmail.com>
2023-08-22 11:20:12 +0500

    day 2

--- 2022/day2/lib.py
+++ 2022/day2/lib.py
@@ -1,34 +1,61 @@
-
-
 def check(a,b):
     pass 
 
 
+def outcome(a, b):
+    opp = relate(a)
+    play = relate(b)
+
+    
+
+    return (points(play, win_loss(opp, play)))
+
+     
+    
+
+
+
+
+def points(your_choice, result):
+    choice_point = 0
+    if (your_choice == "rock"):
+        choice_point = 1 
+    elif (your_choice =="paper"):
+        choice_point = 2
+    elif( your_choice == "scissors"):
+        choice_point = 3
+    
+    win_loss_point = 0
+
+    if (result == "win"):
+        win_loss_point = 6
+    elif (result =="draw"):
+        win_loss_point = 3
+    elif(result == "loss"):
+        win_loss_point = 0
+    
+    return (choice_point + win_loss_point)
 
 
 
 
 
+def relate(choice):
+    if choice == "A" or choice == "X":
+        return "rock"
+    elif choice == "B" or choice == "Y":
+        return "paper"
+    elif choice == "C" or choice == "Z":
+        return "scissors"
 
 
 
-
-def points(given):
-    if (given == "Rock"):
-        return 1 
-    elif (given =="Paper"):
-        return 2
-    elif( given == "Scissors"):
-        return 3
-
-
-
-
-
-def map(choice):
-    if (choice == ("A" or "X")):
-        return "Rock"
-    elif(choice ==("B" or "Y"))
-        return "Paper"
-    elif(choice == ("C" or "Z")):
-        return "Scissors"
+def win_loss(opponent_choice, player_choice):
+    if opponent_choice == player_choice:
+        return "draw"
+    elif (opponent_choice == "rock" and player_choice == "scissors") or \
+         (opponent_choice == "paper" and player_choice == "rock") or \
+         (opponent_choice == "scissors" and player_choice == "paper"):
+        return "loss"
+    else:
+        return "win"
---
+++ 2022/day2/test.py
@@ -0,0 +1,6 @@
+import lib as lib
+
+
+opponent = "C"
+player = "Z"
+
